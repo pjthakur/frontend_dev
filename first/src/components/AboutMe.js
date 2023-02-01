b083c3524e4c568e33a2cd965a50a80d9fb63c72
[pjthakur] <[priyanshjadon@gmail.com]>
2023-02-01 23:51:41 +0530

    adding ToDo app

---
+++ first/src/components/AboutMe.js
@@ -0,0 +1,4 @@
+const AboutMe = () =>{
+    return <h1>hello</h1>
+}
+export default AboutMe;
---
+++ first/src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = props =>{
+    return(
+        <nav>
+            <ul>
+                <li>{props.one}</li>
+                <li>{props.two}</li>
+                <li>{props.three}</li>
+                <li>{props.four}</li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav;
